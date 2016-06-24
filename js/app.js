1edc65e86ca6b181b4523166d09c0882c97ea0b0
Stanley <Stanley@Stanleys-MacBook-Air.local>
2016-06-24 21:33:15 +0800

    add and apply knockoutjs

--- js/app.js
+++ js/app.js
@@ -95,3 +95,12 @@ function toggleBounce(marker) {
         }, 2000);
     }
 }
+
+
+function AppViewModel() {
+    this.firstName = "Bert";
+    this.lastName = "Bertington";
+}
+
+// Activates knockout.js
+ko.applyBindings(new AppViewModel());
